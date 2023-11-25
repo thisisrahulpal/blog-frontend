02a72d3dc0dc4f32a19e9371822b688b22ed59c8
thisisrahulpal <87530955+thisisrahulpal@users.noreply.github.com>
2023-11-25 22:56:33 +0530

    input box signup

--- src/Components/Authentication/Signup.jsx
+++ src/Components/Authentication/Signup.jsx
@@ -1,27 +1,32 @@
 const signup = () => {
   return (
     <>
-    <div className="flex px-4 items-center">
-      <button className="py-1 mr-9">
-        <i className="fas fa-xmark text-white text-4xl scale-75"></i>
-      </button>
-      <div className="py-3">
-        <p className="font-robot text-xl font-bold">Step 1 of 5 </p>
+      <div className="flex px-4 items-center">
+        <button className="py-1 mr-9">
+          <i className="fas fa-xmark text-white text-4xl scale-75"></i>
+        </button>
+        <div className="py-3">
+          <p className="font-robot text-xl font-bold">Step 1 of 5 </p>
+        </div>
       </div>
-    </div>
-    <div className="px-8">
-    <p className="my-5 text-2xl font-bold" >
-    Create your account
-    </p>
-    <div className="py-3">
-    <input className="p-3 py-4 w-full  font-roboto rounded border bg-black border-gray-600/75" type="text" placeholder="Name"/>
-    </div>
-    <div className="py-3">
-    <input className="p-3 py-4 w-full  font-roboto rounded border bg-black border-gray-600/75" type="text" placeholder="Email"/>
-    </div>
-    </div>
+      <div className="px-8">
+        <p className="my-5 text-2xl font-bold">Create your account</p>
+        <div className="py-3">
+          <input
+            className="p-3 py-4 w-full focus:ring-offset-white focus:outline focus:outline-blue-400 font-roboto rounded border bg-black border-gray-600/75  "
+            type="text"
+            placeholder="Name"
+          />
+        </div>
+        <div className="py-3">
+          <input
+            className="p-3 py-4 w-full focus:ring-offset-white focus:outline focus:outline-blue-400 font-roboto rounded border bg-black border-gray-600/75  "
+            type="text"
+            placeholder="Email"
+          />
+        </div>
+      </div>
     </>
-    
   );
 };
 
