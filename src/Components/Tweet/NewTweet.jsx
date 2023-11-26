74a1b574b3a003208d78aa7a6fa5fa669821d8d4
thisisrahulpal <87530955+thisisrahulpal@users.noreply.github.com>
2023-11-26 21:16:41 +0530

    create tweet added

--- src/Components/Tweet/NewTweet.jsx
+++ src/Components/Tweet/NewTweet.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import tweetDp from "../../assets/faces/user-dp.jpeg";
+import { BiSolidBadgeCheck } from "react-icons/bi";
 
 const NewTweet = () => {
   return (
-    <div>NewTweet</div>
-  )
-}
+    <>
+      <div className="flex items-center mt-1 justify-between px-4">
+        <Link to="/">
+          <button className="mr-9 px-1 py-3">
+            <i class="fa-solid fa-arrow-left text-gray-200"></i>
+          </button>
+        </Link>
 
-export default NewTweet
+        <Link to="/">
+          <button className="w-16 h-8 rounded-full bg-blue-400 font-roboto text-sm font-bold">
+            Post
+          </button>
+        </Link>
+      </div>
+
+      <div className="px-4 pt-3 flex flex-rowpx-4 border-gray-700/75">
+        {/* DP */}
+        <div className="w-14 h-14 mr-3">
+          <img className="rounded-full" src={tweetDp} alt="" />
+        </div>
+        <div className="w-full pb-3 ">
+          <input
+            className="bg-black text-xl p-2 focus:outline-none border-none"
+            type="text"
+            placeholder="What is happening?!"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NewTweet;
