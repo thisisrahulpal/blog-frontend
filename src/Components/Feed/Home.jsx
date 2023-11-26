2d0f727701f932b714d3e185510bbf0ed6c2b56a
thisisrahulpal <87530955+thisisrahulpal@users.noreply.github.com>
2023-11-26 17:29:18 +0530

    add tweet

--- src/Components/Feed/Home.jsx
+++ src/Components/Feed/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import DP from "../../assets/faces/user-dp.jpeg";
-import Tweet from "./Tweet"
-
+import Tweet from "./Tweet";
 
 const Home = () => {
   return (
@@ -36,24 +35,35 @@ const Home = () => {
         </nav>
 
         {/* Tweets */}
-        <Tweet/>
-        
+        <Tweet />
+
         {/* Menubar */}
-        <div className=" text-white p-4 bg-black fixed bottom-0 w-full border-t border-gray-700/75">
+        <div className="flex flex-col  fixed bottom-0 w-full opacity-90">
+          <div className="px-7">
+            <div className="flex justify-end mb-8">
+              {/* New Tweet button */}
+              <button className="bg-blue-500 text-white w-14 h-14 rounded-full">
+                <i class="fa-solid fa-plus fa-xl"></i>
+              </button>
+            </div>
+          </div>
+
           {/* Your bottom bar content goes here */}
-          <div className="flex justify-around">
-            <button>
-              <i className="fa-solid fa-house fa-xl opacity-90"></i>
-            </button>
-            <button>
-              <i className="fa-solid fa-magnifying-glass fa-xl opacity-90"></i>
-            </button>
-            <button>
-              <i className="fa-solid fa-bell fa-xl opacity-90"></i>
-            </button>
-            <button>
-              <i className="fa-regular fa-envelope fa-xl opacity-90"></i>
-            </button>
+          <div className="p-4 bg-black text-white w-full border-t border-gray-700/75">
+            <div className="flex justify-around">
+              <button>
+                <i className="fa-solid fa-house fa-xl opacity-90"></i>
+              </button>
+              <button>
+                <i className="fa-solid fa-magnifying-glass fa-xl opacity-90"></i>
+              </button>
+              <button>
+                <i className="fa-solid fa-bell fa-xl opacity-90"></i>
+              </button>
+              <button>
+                <i className="fa-regular fa-envelope fa-xl opacity-90"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
