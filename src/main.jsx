316fc6bc6de33d68557c24ac032e0705c3ce7f87
thisisrahulpal <rahulgurupal@gmail.com>
2023-12-31 19:10:13 +0530

    redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Provider } from 'react-redux'
+import { store } from '../src/app/store'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId='288082114001-hub28lb80pjqlu4hpntm1a877m12gu4l.apps.googleusercontent.com'>
   <React.StrictMode>
+    <Provider store={store}>
+    <GoogleOAuthProvider clientId='288082114001-hub28lb80pjqlu4hpntm1a877m12gu4l.apps.googleusercontent.com'>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
   </GoogleOAuthProvider>
+    </Provider>
+  </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   css: {
-    postcss: './postcss.config.js',
+    postcss: "./postcss.config.js",
   },
-})
+  
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
