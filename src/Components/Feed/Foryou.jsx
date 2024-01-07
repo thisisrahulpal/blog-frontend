e2a3c3e2ecdea00ee53902bf605ef3dc6a72241c
thisisrahulpal <rahulgurupal@gmail.com>
2024-01-08 00:37:01 +0530

    loading and error handling

--- src/Components/Feed/Foryou.jsx
+++ src/Components/Feed/Foryou.jsx
@@ -19,107 +19,72 @@ const Foryou = () => {
   const [likedPost] = useLikedPostMutation();
   const [retweetPost] = useRetweetPostMutation();
   const [bookmarkPost] = useBookmarkPostMutation();
+  const { data, isError, isLoading, error } = useGetForYouPostQuery();
 
-  const { data, isLoading, isError } = useGetForYouPostQuery();
-
-  if (!data) {
-    return <div>No data available.</div>;
-  }
-
-  const handleLike = (_id) => {
-    const likedPost = postData.find((post) => post._id == _id);
-
-    const updatedData = postData.map((post) =>
-      post._id === likedPost._id
-        ? {
-            ...post,
-            isLike: !post.isLike,
-            likeCount: post.isLike ? post.likeCount - 1 : post.likeCount + 1,
-          }
-        : post
-    );
-    console.log("🚀 ~ updatedData:", updatedData);
-    setData(updatedData);
-    return;
-  };
-
-  const handleRetweet = (_id) => {
-    const retweetedPost = postData.find((post) => post._id == _id);
-
-    const updatedData = postData.map((post) =>
-      post._id === retweetedPost._id
-        ? {
-            ...post,
-            isRetweeted: !post.isRetweeted,
-            retweetCount: post.isRetweeted
-              ? post.retweetCount - 1
-              : post.retweetCount + 1,
-          }
-        : post
-    );
-    console.log("🚀 ~ updatedData:", updatedData);
-    setData(updatedData);
-    return;
-  };
 
   return (
     <>
-      {/* <Skeleton /> */}
-      {data.map((postData, index) => (
-        <div
-          key={postData._id}
-          className="px-4 pt-3 flex flex-rowpx-4 border-b border-gray-700/75"
-        >
-          {/* DP */}
-          <div className="w-10 h-10 mr-3">
-            <img className="rounded-full" src={tweetDp} alt="" />
-          </div>
-          <div className="w-full pb-3 ">
-            {/* Head */}
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <p className="text-md font-bold pr-1">{postData.name}</p>
-                <BiSolidBadgeCheck className="text-blue-400 scale-125" />
-                <p className="font-roboto text-md text-gray-500 pl-1">
-                  {postData.username}
-                </p>
-                <p className="font-roboto text-md text-gray-500 px-1">.</p>
-                <p className="font-roboto text-md text-gray-500">1h</p>
+      {isLoading ? (
+        <Skeleton />
+      ) : isError ? (
+        <div>No data available.</div>
+      ) : (
+        data.map((postData, index) => (
+          <div
+            key={postData._id}
+            className="px-4 pt-3 flex flex-rowpx-4 border-b border-gray-700/75"
+          >
+            {/* DP */}
+            <div className="w-10 h-10 mr-3">
+              <img className="rounded-full" src={tweetDp} alt="" />
+            </div>
+            <div className="w-full pb-3 ">
+              {/* Head */}
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <p className="text-md font-bold pr-1">{postData.name}</p>
+                  <BiSolidBadgeCheck className="text-blue-400 scale-125" />
+                  <p className="font-roboto text-md text-gray-500 pl-1">
+                    {postData.username}
+                  </p>
+                  <p className="font-roboto text-md text-gray-500 px-1">.</p>
+                  <p className="font-roboto text-md text-gray-500">1h</p>
+                </div>
+                <div>
+                  <i className="fa-solid fa-ellipsis fa-md text-gray-500"></i>
+                </div>
               </div>
-              <div>
-                <i className="fa-solid fa-ellipsis fa-md text-gray-500"></i>
+              {/* Body */}
+              <div className="font-roboto text-base opacity-90">
+                <p>{postData.post}</p>
+              </div>
+              {/* buttons */}
+              <div className="flex justify-between py-2 w-full  opacity-90 text-gray-400">
+                {/* comments */}
+                <Comment />
+                <Retweet
+                  isRetweet={postData.isRetweet}
+                  count={postData.reTweetCount}
+                  onRetweet={retweetPost}
+                  id={postData._id}
+                />
+                <Like
+                  count={postData.likeCount}
+                  isLike={postData.isLike}
+                  id={postData._id}
+                  likedPost={likedPost}
+                />
+                <Views count={postData.viewCount} />
+                <Bookmark
+                  isBookmark={postData.isBookmark}
+                  bookmarkPost={bookmarkPost}
+                  id={postData._id}
+                />
               </div>
             </div>
-            {/* Body */}
-            <div className="font-roboto text-base opacity-90">
-              <p>{postData.post}</p>
-            </div>
-            {/* buttons */}
-            <div className="flex justify-between py-2 w-full  opacity-90 text-gray-400">
-              {/* comments */}
-              <Comment />
-              <Retweet
-                isRetweet={postData.isRetweet}
-                count={postData.reTweetCount}
-                onRetweet={retweetPost}
-                id={postData._id}
-              />
-              <Like
-                count={postData.likeCount}
-                isLike={postData.isLike}
-                id={postData._id}
-                likedPost={likedPost}
-              />
-              <Views count={postData.viewCount} />
-              <Bookmark
-                isBookmark={postData.isBookmark}
-                bookmarkPost={bookmarkPost}
-                id={postData._id}
-              />
-            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </>
   );
 };
